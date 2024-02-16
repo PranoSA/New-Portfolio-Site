510109ce7fe18543173f51936056e5ed669884ab
PranoSA <pcadler@gmail.com>
2024-02-16 14:19:58 -0800

    Parse Tree Representation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,12 +15,15 @@ import {
   PrecedenceArgument,
   genAST,
   runAST,
+  TreeForJS3,
+  TreeForJs3,
 } from '@pranosa/makebelieve_parse_precedence';
 import * as d3 from 'd3';
 
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
 import CompilerTile from '@/components/compilers';
+import { ASTBranch } from '@pranosa/makebelieve_parse_precedence/dist/cjs/vm';
 
 export default function Home() {
   const [openPanels, setPanels] = useState<boolean[]>([
@@ -68,8 +71,165 @@ export default function Home() {
   });
   const [showStack, setShowStack] = useState<boolean>(false);
   const [vm, setVM] = useState<VM | null>(null);
+  const [tree, setTree] = useState<TreeForJs3>({
+    value: 'root',
+    children: [],
+  });
 
-  const d3ref = useRef<SVGSVGElement>(null);
+  const d3Ref = useRef<HTMLDivElement>(null);
+
+  let stepintraverse = 0;
+
+  function setHierarchyAttributes(node: any, name = 'default', value = 0) {
+    // Set the name and value properties
+    const castasnode = node as TreeForJs3;
+    node.name = castasnode.value || name;
+    node.value = node.value || value + 'VALUE';
+
+    // Recursively set the properties for each child
+    if (Array.isArray(node.children)) {
+      //Only Two Children , start with 1 and then 0
+      const secondChild = node.children[0];
+      if (secondChild) setHierarchyAttributes(secondChild, `child2`, 1);
+      const firstChild = node.children[1];
+      if (firstChild) setHierarchyAttributes(firstChild, `child1`, 0);
+
+      /*node.children.forEach((child: any, index: any) => {
+        setHierarchyAttributes(child, `child${index + 1}`, index + 1);
+      });*/
+    }
+    node.step = stepintraverse;
+    stepintraverse++;
+
+    const stack_size = vm?.program_states[stepintraverse].stack.length;
+    if (!stack_size || stack_size === 0) return;
+
+    node.calculated_value =
+      vm?.program_states[stepintraverse].stack[stack_size - 1];
+    console.log('Node Info');
+    console.log(node.calculated_value);
+    console.log(vm?.program_states[stepintraverse].stack[stack_size || 1 - 1]);
+    console.log('Stack Info');
+    console.log(stepintraverse);
+    console.log(stack_size);
+    console.log(vm?.program_states[stepintraverse].stack);
+
+    //How to associate current Step with the node
+
+    node.style = currentStep == value ? 'fill: red' : 'fill: white';
+  }
+
+  //setHierarchyAttributes(yourTree); // Replace 'yourTree' with your actual tree
+
+  useEffect(() => {
+    if (!showStack) return;
+    if (tree && d3Ref.current) {
+      // Clear the previous tree
+
+      // Clear the previous tree
+      d3.select(d3Ref.current).selectAll('*').remove();
+
+      const svg = d3
+        .select(d3Ref.current)
+        .append('svg')
+        .attr('width', 600)
+        .attr('height', 600);
+
+      const modifiedTree = { ...tree };
+
+      stepintraverse = 0;
+
+      setHierarchyAttributes(modifiedTree);
+
+      // Convert the tree data into a d3 hierarchy
+      const root = d3.hierarchy(modifiedTree);
+
+      // Create a tree layout and assign the size
+      const treeLayout = d3.tree().size([500, 500]);
+
+      // Assign the computed layout to root
+      treeLayout(root as any);
+
+      const linkGenerator = {
+        //@ts-ignore
+        source: (d) => [d.source.y + 10, d.source.x + 10],
+        //@ts-ignore
+        target: (d) => [d.target.y + 10, d.target.x + 10],
+      };
+
+      // Render the links
+      root.links().forEach((link) => {
+        svg
+          .append('line')
+          .attr('x1', linkGenerator.source(link)[0])
+          .attr('y1', linkGenerator.source(link)[1])
+          .attr('x2', linkGenerator.target(link)[0])
+          .attr('y2', linkGenerator.target(link)[1])
+          .style('stroke', 'black');
+      });
+
+      // Render the nodes
+      root.descendants().forEach((d, i) => {
+        svg
+          .append('circle')
+          //@ts-ignore
+          .attr('cx', d.y + 30) // Use d.y for the horizontal position
+          //@ts-ignore
+          .attr('cy', d.x + 20) // Use d.x for the vertical position
+          .attr('r', 25) // Set the radius of the circle
+          //.style('fill', 'white'); //Find if current step is refferring to this node
+          //@ts-ignore
+          .style('fill', currentStep == d.data.step ? 'red' : 'yellow'); //Find if current step is refferring to this node
+
+        //@ts-ignore
+        const current_step_text =
+          //@ts-ignore
+          currentStep < d.data.step + 1 //d.data.step
+            ? //@ts-ignore
+              currentStep == d.data.step
+              ? d.children?.length == 2
+                ? //@ts-ignore
+                  `${d.children[0].data.calculated_value}${d.data.name}${d.children[1].data.calculated_value}`
+                : //@ts-ignore
+                  d.data.name
+              : //@ts-ignore
+                d.data.name && d.data.name
+            : //@ts-ignore
+              d.data.calculated_value;
+        console.log('Current Step Text ' + current_step_text);
+
+        svg
+          .append('text')
+          //@ts-ignore
+          .attr('x', d.y + 0) // Position the label to the right of the node
+          //@ts-ignore
+          .attr('y', d.x + 0) // Position the label slightly below the node
+          //@ts-ignore
+          .text(
+            current_step_text
+            //@ts-ignore
+            /*currentStep > d.data.step
+              ? //@ts-ignore
+                currentStep === d.data.step
+                ? current_step_text
+                : //@ts-ignore
+                  d.data.calculated_value
+              : //@ts-ignore
+                d.data.name*/
+          ) //
+          //@ts-ignore
+          //.text(d.data.name) //
+          .style('font-size', '35px')
+          .style('font-weight', 'bold');
+      });
+
+      const nodeCount = root.descendants().length;
+      console.log(`Number of nodes: ${nodeCount}`); // Log the number of nodes in the tree
+
+      // Render the tree here using d3's selection and data binding methods
+      // This will depend on the structure of your tree data and how you want to render it
+    }
+  }, [tree, currentStep, showStack]);
 
   const [precedenceArguments, setPrecedenceArguments] =
     useState<PrecedenceArgument>({
@@ -100,6 +260,7 @@ export default function Home() {
   const runCompiler = () => {
     const tokens = Lexer(sourceCode);
     const parser = new Parser(tokens, precedenceArguments, 0);
+    setCurrentStep(0);
     parser.beginParsing();
 
     const new_vm: VM = {
@@ -107,18 +268,29 @@ export default function Home() {
       top: 0,
       program: parser.bytecode,
       ip: 0,
+      state_branch: [],
+      program_states: [],
     };
 
     setVM(new_vm);
 
-    runProgram(new_vm);
+    const result = runProgram(new_vm) || 0;
 
     const vm_for_ast: VM = {
       stack: [],
       top: 0,
       program: parser.bytecode,
       ip: 0,
+      state_branch: [],
+      program_states: [],
     };
+
+    const ast = genAST(vm_for_ast);
+
+    setTree(TreeForJS3(ast[0]));
+    console.log('tree');
+    console.log(TreeForJS3(ast[0]));
+    console.log(ast);
 
     console.log(new_vm.stack[new_vm.top - 1]);
 
@@ -130,7 +302,6 @@ export default function Home() {
 
     //setAST(ast[0]);
 
-    const result = new_vm.stack[new_vm.top - 1];
     setResult(result);
   };
 
@@ -323,8 +494,9 @@ export default function Home() {
           </div>
 
           <div className="w-full p-4 text-center">
-            {openPanels[2]
-              ? CompilerTile({
+            {openPanels[2] ? (
+              <div className="flex flex-wrap">
+                {CompilerTile({
                   sourceCode: sourceCode,
                   result: result,
                   precedenceArguments: precedenceArguments,
@@ -342,8 +514,12 @@ export default function Home() {
                   currentStep: currentStep,
                   setCurrentStep: (step: number) =>
                     setCurrentStep(step + currentStep),
-                })
-              : null}
+                })}
+                <div className="w-full p-4 text-center" ref={d3Ref}>
+                  <h1> Tree ? </h1>
+                </div>
+              </div>
+            ) : null}
           </div>
         </div>
 
