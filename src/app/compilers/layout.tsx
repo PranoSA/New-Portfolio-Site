2e9a5140ae4249074309b8eb7f2efff67649689e
wadler@socal.rr.com <wadler@socal.rr.com>
2024-02-18 19:09:20 -0800

    backout color

--- src/app/compilers/layout.tsx
+++ src/app/compilers/layout.tsx
@@ -11,7 +11,6 @@ export const metadata = {
     children: React.ReactNode;
   }) {
     return (
-     <div class="h-14 bg-gradient-to-r from-sky-500 to-indigo-500"></div>
       <html lang="en">
       <body>
         <div className="fixed top-0 left-0 p-4">
