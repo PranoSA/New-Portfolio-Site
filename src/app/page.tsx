30dcedc9105a1bad4f974bbd79011190392c2a90
PranoSA <pcadler@gmail.com>
2024-08-02 09:33:37 -0700

    Parameterized Articles List

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,86 @@ import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 
+type ArticleDescription = {
+  URL: string;
+  Description: string;
+  Title: string;
+};
+
+const articleDescriptions: ArticleDescription[] = [
+  {
+    Title: 'OpenVPN Networking',
+    Description: `                In this article I created separate networks on a single linux
+                for Wireshark) to capture packets on one network interface, and
+                then use Wireshark to analyze the packets. I also use OpenVPN to
+                create a VPN Tunnel between two machines, and then use Wireshark
+                to capture packets on the VPN Tunnel.`,
+    URL: 'https://articles.compressibleflowcalculator.com/OpenVPN',
+  },
+  {
+    Title: 'Video Transcoding With FFMEPG',
+    Description: `In this article I analyze a popular steraming protocol built on HTTP called 
+    MPEG-DASH (Dynamic Adaptive Streaming over HTTP). I then use FFMEPG to transcode a video 
+    file to what are called mp4 fragments (.mps) and then create a manifest file (.mpd) that
+     contains meta-information of the chosen streams and encoding bit rates for streaming options
+      for each media stream (audio and video). You can then use a simple Javascript File on an HTML
+       file to serve the video to a client and see throug
+    h the Networking Console how the video matches with the contents of the manifest file.`,
+    URL: 'https://articles.compressibleflowcalculator.com/MPEG-DASH',
+  },
+  {
+    Title: 'Digital Maps',
+    Description: `                In this article I introduce the concept of Digital Maps, and how
+                they compare to Image Maps and SVG Maps. Image Maps give visual
+                information to the user and may involve pre-rendered
+                geographical information about a variables, SVG Maps on the web
+                allow native javascript interaction with image, but digital maps
+                offer high degree of variant information handling and display
+                capabilities, allowing the use of various raster data formats,
+                vector data formats, and tiled vector and raster data formats,
+                as well as rich display capabilities (Heat maps, contour maps,
+                clustering, etc.), and interaction with a geoServer to render
+                new layers on a map.`,
+    URL: 'https://articles.compressibleflowcalculator.com/Digital_Maps',
+  },
+  {
+    Title: 'DNSSEC and Reverse DNS Part 1',
+    Description: `                In this first part of the series, we will set up the basic tools
+                with Docker to run a parent and child nameserver as well as a
+                Dig DNS client. We Will also go over reading the output of the
+                Dig Command and concepts such as DNS flags, EDNS, and TShark/
+                Wireshark. We will then talk about inspecting the DNS packets
+                inside of wireshark using tshark to capture the packets on the
+                DNS client interface. We will then inspect the idea of zone
+                files and the records contained inside of them. These articles
+                will not teach about DNS and DNS-SEC basic conepts but Will
+                refer to other articles on the topic, and rather lay groundwork
+                for experimentation with DNS in a easy to use environment.We
+                will also go over features of the dig command`,
+    URL: 'https://articles.compressibleflowcalculator.com/DNSSEC_PART_1',
+  },
+  {
+    Title: 'DNSSEC and Reverse DNS Part 2',
+    Description: `                 In this article, we will build off the 2nd and fix the DNS-SEC
+                implimentation by adding the appropriate DS record to the parent
+                server to properly authenticate the zone transfer. Here, we will
+                be able to use tshark as well as the dig response to see the
+                verification of the DNS in the EDNS flags as well as the DNSSEC
+                flags in the request.`,
+    URL: 'https://articles.compressibleflowcalculator.com/DNSSEC_PART_2',
+  },
+  {
+    Title: 'Introduction to Timestamps in Postgres',
+    Description: `In thie article, I will discuss the basic premises of Clocks, Timers, and Time Zones and 
+    Concepts such as UTC, IAT, Unix Timestamps, and Timestamp Formats. Then I will go over examples of the time 
+    library in GO, and talk about the "awareness" time libraries have with dealing with time zones and locations when
+    making time calculations. Then I will introduce the different Time and Date types in Postgres, how they work, and examples
+    of queries to make time calculations such as Evaluating a Timestamp at a particular Time Zone (If It Has Time Zone Information), 
+    Calculating the Time Between Events, and extracting second, minute, hour, day, month, year, and epoch information from a timestamp.`,
+    URL: 'https://articles.compressibleflowcalculator.com/Timestamp_Introduction_Postgres',
+  },
+];
+
 export default function Home() {
   const [openPanels, setPanels] = useState<boolean[]>([
     false,
@@ -23,7 +103,9 @@ export default function Home() {
     false,
   ]);
 
-  const [openAriclePanels, setArticlePanels] = useState<boolean[]>([false]);
+  const [openAriclePanels, setArticlePanels] = useState<boolean[]>(
+    articleDescriptions.map(() => false)
+  );
   const [loading, setLoading] = useState<boolean>(false);
   //Encoding Mode, (Decimal or Hexadecimal)
 
@@ -244,196 +326,41 @@ export default function Home() {
           <h1 className="w-full text-5xl font-bold "> Articles </h1>
         </div>
 
-        <div className="w-full md:w-2/3 p-20 flex flex-wrap p-4 text-center min-h-20 cursor-pointer hover:bg-blue-100 hover:shadow-lg transform hover:scale-105 transition-all duration-200 ease-in-out">
-          <h1
-            className="w-full text-3xl font-bold"
-            onClick={() => {
-              setArticlePanels([
-                !openAriclePanels[0],
-                ...openAriclePanels.slice(1),
-              ]);
-            }}
-          >
-            {' '}
-            OpenVPN Networking
-          </h1>
-          {openAriclePanels[0] ? (
-            <div className="m-5">
-              <p className="w-full pb-4 ">
-                In this article I created separate networks on a single linux
-                for Wireshark) to capture packets on one network interface, and
-                then use Wireshark to analyze the packets. I also use OpenVPN to
-                create a VPN Tunnel between two machines, and then use Wireshark
-                to capture packets on the VPN Tunnel.
-              </p>
-              <div className="w-full w-flex flex-wrap p-4 text-center h-15"></div>
-              <a
-                href="https://articles.compressibleflowcalculator.com/OpenVPN"
-                className="p-5 text-center bg-blue-200 rounded m-5"
+        {articleDescriptions.map((article, index) => {
+          return (
+            <div
+              key={index}
+              className="w-full md:w-2/3 p-20 flex flex-wrap p-4 text-center min-h-20 cursor-pointer hover:bg-blue-100 hover:shadow-lg transform hover:scale-105 transition-all duration-200 ease-in-out"
+            >
+              <h1
+                key={index}
+                className="w-full text-3xl font-bold"
+                onClick={() => {
+                  setArticlePanels([
+                    ...openAriclePanels.slice(0, index),
+                    !openAriclePanels[index],
+                    ...openAriclePanels.slice(index + 1),
+                  ]);
+                }}
               >
-                Read Article{' '}
-              </a>
+                {' '}
+                {article.Title}
+              </h1>
+              {openAriclePanels[index] ? (
+                <div className="m-5">
+                  <p className="w-full pb-4 ">{article.Description}</p>
+                  <div className="w-full w-flex flex-wrap p-4 text-center h-15"></div>
+                  <a
+                    href={article.URL}
+                    className="p-5 text-center bg-blue-200 rounded m-5"
+                  >
+                    Read Article{' '}
+                  </a>
+                </div>
+              ) : null}
             </div>
-          ) : null}
-        </div>
-        <div className="w-full md:w-1/2 p-20 flex m-10 flex-wrap p-4 text-center min-h-20 cursor-pointer hover:bg-blue-100 hover:shadow-lg transform hover:scale-105 transition-all duration-200 ease-in-out">
-          <h1
-            className="w-full text-3xl font-bold"
-            onClick={() => {
-              setArticlePanels([
-                false,
-                !openAriclePanels[1],
-                ...openAriclePanels.slice(2),
-              ]);
-              console.log(openAriclePanels);
-            }}
-          >
-            {' '}
-            Video Transcoding With FFMEPG
-          </h1>
-          {openAriclePanels[1] ? (
-            <div className="m-5">
-              <p className="w-full pb-4 ">
-                In this article I analyze a popular steraming protocol built on
-                HTTP called MPEG-DASH (Dynamic Adaptive Streaming over HTTP). I
-                then use FFMEPG to transcode a video file to what are called mp4
-                fragments (.mps) and then create a manifest file (.mpd) that
-                contains meta-information of the chosen streams and encoding bit
-                rates for streaming options for each media stream (audio and
-                video). You can then use a simple Javascript File on an HTML
-                file to serve the video to a client and see through the
-                Networking Console how the video matches with the contents of
-                the manifest file.
-              </p>
-              <div className="w-full w-flex flex-wrap p-4 text-center h-15"></div>
-              <a
-                href="https://articles.compressibleflowcalculator.com/MPEG-DASH"
-                className="p-5 text-center bg-blue-200 rounded"
-              >
-                Read Article{' '}
-              </a>
-            </div>
-          ) : null}
-        </div>
-        <div className="w-full md:w-2/3 p-20 flex flex-wrap p-4 text-center min-h-20 cursor-pointer hover:bg-blue-100 hover:shadow-lg transform hover:scale-105 transition-all duration-200 ease-in-out">
-          <h1
-            className="w-full text-3xl font-bold"
-            onClick={() => {
-              setArticlePanels([
-                false,
-                false,
-                !openAriclePanels[2],
-                ...openAriclePanels.slice(3),
-              ]);
-            }}
-          >
-            {' '}
-            Digital Maps
-          </h1>
-          {openAriclePanels[2] ? (
-            <div className="m-5">
-              <p className="w-full pb-4 ">
-                In this article I introduce the concept of Digital Maps, and how
-                they compare to Image Maps and SVG Maps. Image Maps give visual
-                information to the user and may involve pre-rendered
-                geographical information about a variables, SVG Maps on the web
-                allow native javascript interaction with image, but digital maps
-                offer high degree of variant information handling and display
-                capabilities, allowing the use of various raster data formats,
-                vector data formats, and tiled vector and raster data formats,
-                as well as rich display capabilities (Heat maps, contour maps,
-                clustering, etc.), and interaction with a geoServer to render
-                new layers on a map.
-              </p>
-              <div className="w-full w-flex flex-wrap p-4 text-center h-15"></div>
-              <a
-                href="https://articles.compressibleflowcalculator.com/Digital_Maps"
-                className="p-5 text-center bg-blue-200 rounded m-5"
-              >
-                Read Article{' '}
-              </a>
-            </div>
-          ) : null}
-        </div>
-        <div className="w-full md:w-2/3 p-20 flex flex-wrap p-4 text-center min-h-20 cursor-pointer hover:bg-blue-100 hover:shadow-lg transform hover:scale-105 transition-all duration-200 ease-in-out">
-          <h1
-            className="w-full text-3xl font-bold"
-            onClick={() => {
-              setArticlePanels([
-                false,
-                false,
-                false,
-                !openAriclePanels[3],
-                ...openAriclePanels.slice(4),
-              ]);
-            }}
-          >
-            {' '}
-            DNSSEC and Reverse DNS Part 1
-          </h1>
-          {openAriclePanels[3] ? (
-            <div className="m-5">
-              <p className="w-full pb-4 ">
-                In this first part of the series, we will set up the basic tools
-                with Docker to run a parent and child nameserver as well as a
-                Dig DNS client. We Will also go over reading the output of the
-                Dig Command and concepts such as DNS flags, EDNS, and TShark/
-                Wireshark. We will then talk about inspecting the DNS packets
-                inside of wireshark using tshark to capture the packets on the
-                DNS client interface. We will then inspect the idea of zone
-                files and the records contained inside of them. These articles
-                will not teach about DNS and DNS-SEC basic conepts but Will
-                refer to other articles on the topic, and rather lay groundwork
-                for experimentation with DNS in a easy to use environment.We
-                will also go over features of the dig command
-              </p>
-              <div className="w-full w-flex flex-wrap p-4 text-center h-15"></div>
-              <a
-                href="https://articles.compressibleflowcalculator.com/DNSSEC_PART_1"
-                className="p-5 text-center bg-blue-200 rounded m-5"
-              >
-                Read Article{' '}
-              </a>
-            </div>
-          ) : null}
-        </div>
-        <div className="w-full md:w-2/3 p-20 flex flex-wrap p-4 text-center min-h-20 cursor-pointer hover:bg-blue-100 hover:shadow-lg transform hover:scale-105 transition-all duration-200 ease-in-out">
-          <h1
-            className="w-full text-3xl font-bold"
-            onClick={() => {
-              setArticlePanels([
-                false,
-                false,
-                false,
-                false,
-                !openAriclePanels[4],
-                ...openAriclePanels.slice(5),
-              ]);
-            }}
-          >
-            {' '}
-            DNSSEC and Reverse DNS Part 2
-          </h1>
-          {openAriclePanels[4] ? (
-            <div className="m-5">
-              <p className="w-full pb-4 ">
-                In this article, we will build off the 2nd and fix the DNS-SEC
-                implimentation by adding the appropriate DS record to the parent
-                server to properly authenticate the zone transfer. Here, we will
-                be able to use tshark as well as the dig response to see the
-                verification of the DNS in the EDNS flags as well as the DNSSEC
-                flags in the request.
-              </p>
-              <div className="w-full w-flex flex-wrap p-4 text-center h-15"></div>
-              <a
-                href="https://articles.compressibleflowcalculator.com/DNSSEC_PART_2"
-                className="p-5 text-center bg-blue-200 rounded m-5"
-              >
-                Read Article{' '}
-              </a>
-            </div>
-          ) : null}
-        </div>
+          );
+        })}
       </div>
     </main>
   );
